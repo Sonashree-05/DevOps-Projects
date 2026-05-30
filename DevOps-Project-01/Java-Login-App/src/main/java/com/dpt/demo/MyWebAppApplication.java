41e9bc328f0f2d10a30402d10421acdd69edbbac
Sonashree-05 <123352656+Sonashree-05@users.noreply.github.com>
2026-05-30 12:16:21 +0530

    Update MyWebAppApplication.java

--- DevOps-Project-01/Java-Login-App/src/main/java/com/dpt/demo/MyWebAppApplication.java
+++ DevOps-Project-01/Java-Login-App/src/main/java/com/dpt/demo/MyWebAppApplication.java
@@ -1,13 +1,19 @@
-package com.dpt.demo;
+package com.devopsrealtime.dptweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyWebAppApplication {
+public class DptwebApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyWebAppApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DptwebApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(DptwebApplication.class, args);
+    }
 }
